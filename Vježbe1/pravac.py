4ca9a66a573059e3b89cc979d1ef901aeb3c95dd
andrijamisurac1 <amisurac@pmfst.hr>
2026-03-17 12:44:30 +0100

    ispravak unosa

--- Vježbe1/pravac.py
+++ Vježbe1/pravac.py
@@ -1,3 +1,16 @@
-x=float(input("Unesite x koordinatu: "))
-y=float(input("Unesite y koordinatu: "))
+while True:
+    try:
+        x = float(input("Unesite x koordinatu: "))
+        break
+    except ValueError:
+        print("Pogrešan unos! Molimo unesite broj.")
+
+while True:
+    try:
+        y = float(input("Unesite y koordinatu: "))
+        break
+    except ValueError:
+        print("Pogrešan unos! Molimo unesite broj.")
+
 print("f(x) =", x)
+
